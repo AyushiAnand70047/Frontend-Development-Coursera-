38eb3ee9232240ece8a2f1a17fdedc5eccbe8e53
AyushiAnand70047 <ayushianand088@gmail.com>
2024-01-28 04:44:32 +0530

    frontend development

---
+++ Programming With JavaScript/Module 2/Array.js
@@ -0,0 +1,4 @@
+var train1 = ["wheat", "barley", "potato", "salt", "rocks"];
+for(var i=0;i<train1.length;i++){
+    console.log(train1[i]);
+}
---
+++ Programming With JavaScript/Module 2/function.js
@@ -0,0 +1,6 @@
+function addTwoNums(a, b){
+    var c = a + b;
+    console.log(c);
+}
+addTwoNums(2,4);
+addTwoNums(7,10);
