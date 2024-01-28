ee10ced0586d7fe6c6ac411ab7cedb3c6a5c6a70
AyushiAnand70047 <ayushianand088@gmail.com>
2024-01-28 06:10:12 +0530

    frontend development

---
+++ Programming With JavaScript/Module 2/typeofOperator.js
@@ -0,0 +1,10 @@
+var test1 = typeof('what is this?');
+var test2 = typeof(10);
+var test3 = typeof(3.14);
+var test4 = typeof(true);
+var test5 = typeof(false);
+var test6 = typeof(1 < 2);
+var test7 = typeof([1,2,3]);
+var test8 = typeof({ firstProperty: 1});
+var test9 = typeof(function abc(){ console.log('abc'); });
+console.log(test1,test2,test3,test4,test5,test6,test7,test8,test9);
